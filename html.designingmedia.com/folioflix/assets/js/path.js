b4ad1f577559e8071c605a74875b85428543ab11
SarthakMittal1 <mittalsarthak863@gmail.com>
2023-12-31 17:55:02 +0530

    as

---
+++ html.designingmedia.com/folioflix/assets/js/path.js
@@ -0,0 +1,8 @@
+// Get the list item element by its ID
+var blogListItem = document.getElementById('blogLink');
+
+// Add a click event listener to the list item
+blogListItem.addEventListener('click', function() {
+    // Redirect to the desired URL when the list item is clicked
+    window.location.href = 'https://medium.com/@mittalsarthak863';
+});
